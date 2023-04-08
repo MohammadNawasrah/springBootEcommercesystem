7b144aa8dcf4139a70c7523852a4592378239b44
Mohammad Al Nawasrah <nawasrahmohammad2000@gmail.com>
2023-04-08 15:37:32 +0300

    add sign up api

---
+++ src/main/java/in/nawasrah/ecommercesystemAPI/database/DB.java
@@ -0,0 +1,16 @@
+package in.nawasrah.ecommercesystemAPI.database;
+
+import org.springframework.stereotype.Repository;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+@Repository
+public interface DB {
+    Connection connection(String connectionString);
+    boolean createTable(String sql,Connection con);
+    ResultSet select(String sql, Connection con);
+    boolean insert(String sql,Connection con);
+    boolean delete(String sql,Connection con);
+    boolean update(String sql,Connection con);
+
+}
---
+++ src/main/java/in/nawasrah/ecommercesystemAPI/model/User.java
@@ -0,0 +1,18 @@
+package in.nawasrah.ecommercesystemAPI.model;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+public class User {
+    private long users_id;
+    private String users_name;
+    private String users_email;
+    private int users_verifycode;
+    private boolean users_approve;
+    private Date user_create;
+}
