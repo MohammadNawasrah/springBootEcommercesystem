4a6d4650194e951687b026ebee1445d985a8bd04
Mohammad Al Nawasrah <nawasrahmohammad2000@gmail.com>
2023-04-08 15:33:17 +0300

    initializer spring boot project

---
+++ src/main/java/in/nawasrah/ecommercesystemAPI/EcommercesystemApiApplication.java
@@ -0,0 +1,13 @@
+package in.nawasrah.ecommercesystemAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercesystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercesystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/nawasrah/ecommercesystemAPI/EcommercesystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nawasrah.ecommercesystemAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercesystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
